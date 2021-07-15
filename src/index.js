33e67160547bc42e21e1ea19ddb893c3e6c36bf2
garikdallari <garikdallari@gmail.com>
2021-07-15 17:52:11 +0300

    add styles

--- src/index.js
+++ src/index.js
@@ -28,16 +28,18 @@ async function onSearch(e) {
    
 
     try {
-        const hits = await imageApiService.fetchImages()
-        appendImageMarkup(hits);
+        const res = await imageApiService.fetchImages()
+        appendImageMarkup(res.hits);
+
+       Notiflix.Notify.success(`Hooray! We found ${res.totalHits} images.`);
     
-        if (hits.length === 0) {
+        if (res.hits.length === 0) {
         refs.loadMoreBtn.classList.add('hidden')
             Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
             return;
         };
         
-        if (hits.length > hits.totalHits) {
+        if (res.hits.length > res.totalHits) {
          Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
         };
         refs.loadMoreBtn.classList.remove('hidden');
@@ -50,8 +52,8 @@ async function onSearch(e) {
 async function onLoadMore() {
     
     try {
-        const hits = await imageApiService.fetchImages();
-        appendImageMarkup(hits)
+        const res = await imageApiService.fetchImages();
+        appendImageMarkup(res.hits)
       
     } catch (error) {
         console.log(error);
@@ -59,8 +61,8 @@ async function onLoadMore() {
 }
 
 
-function appendImageMarkup(hits) {
-    refs.imageContainer.insertAdjacentHTML('beforeend', imageCardTpl(hits))
+function appendImageMarkup(data) {
+    refs.imageContainer.insertAdjacentHTML('beforeend', imageCardTpl(data))
 }
 
 function clearImgContainer() {
--- src/js/image-service.js
+++ src/js/image-service.js
@@ -9,12 +9,12 @@ export default class ImageApiService {
     }
 
     async fetchImages() {
-        const url = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=8`;
+        const url = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
         
         const response = await axios.get(url);
      
         this.page += 1;
-        return response.data.hits
+        return response.data
     }
 
       resetPage() {
